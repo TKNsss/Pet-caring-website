d7b5ea07d8af7ef325579092c8ec5159daf43789
NamPhan <namphan10102k4@gmail.com>
2025-03-13 07:37:23 +0700

    update galleryHeader

--- Pet-caring-website/ClientApp/src/components/GalleryPage/GalleryHeader.jsx
+++ Pet-caring-website/ClientApp/src/components/GalleryPage/GalleryHeader.jsx
@@ -12,9 +12,9 @@ const GalleryHeader = () => {
       <img src={pictureMain2} alt="" className="block [clip-path:circle(75%_at_50%_0%)] h-[60vh] w-full ml-auto sm:h-[85vh] sm:[clip-path:circle(68%_at_50%_0%)] lg:hidden"/>
       <div className="text-center lg:w-2/5">
           <div className="lg:inline-block">
-            <h1 style={{fontFamily: "chewy"}} className="text-center text-[40px] leading-[67.95px] sm:text-[50px] md:text-[60px] lg:pt-[72px] lg:text-left lg:text-[52px]">OUR HAPPY MOMENTS</h1>
-            <p className="text-center text-[20px] mt-[2%] leading-[30px] pb-[30px] md:text-[30px] lg:text-left lg:mt-[30px]">We bring joys and loves to your besties</p>
-            <button style={{fontFamily: "chewy"}} className='block text-center text-[23px] leading-[30.05px] w-[227px] h-[54px] bg-[#E4813A] text-white m-auto mb-[10%] rounded-[25px] shadow-[1px_4px_5px_rgba(0,0,0,0.3)]'>
+            <h1 style={{fontFamily: "chewy"}} className="text-center text-[40px] leading-[67.95px] sm:text-[50px] md:text-[60px] lg:mt-[82px] lg:text-[42px] xl:mt-[72px] xl:text-left xl:text-[52px]">OUR HAPPY MOMENTS</h1>
+            <p className="text-center text-[20px] mt-[2%] leading-[30px] pb-[30px] md:text-[30px] lg:text-[20px] lg:mt-[30px] xl:text-left xl:mt-[30px]">We bring joys and loves to your besties</p>
+            <button style={{fontFamily: "chewy"}} className='block text-center text-[23px] leading-[30.05px] w-[227px] h-[54px] bg-[#E4813A] text-white m-auto mb-[10%] rounded-[25px] shadow-[1px_4px_5px_rgba(0,0,0,0.3)] lg:m-0 lg:mt-[30px]'>
               <i className='fa-solid fa-paw mr-[5px]'></i>
               Request Services
             </button>
